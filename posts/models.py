87a516d34ad7d872351ce22255237fc706db8ad0
Taras Piont <taras.piont@gmail.com>
2020-09-30 20:37:12 +0200

    only first step

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User  = get_user_model()
+
+class Post(models.Model):
+    text  = models.TextField()
+    pub_date = models.DateTimeField('date published',auto_now_add=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='post')
+
+
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+
+def index(request):
+    latest = Post.objects.order_by('-pub_date')[:11]
+    return render(request,'index.html',{'posts':latest})
+
+
+
+
+
